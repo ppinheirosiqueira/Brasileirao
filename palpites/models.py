78d941035414446b479ef58981adbcf862012120
Pedro Pinheiro <49202702+ppinheirosiqueira@users.noreply.github.com>
2023-04-17 23:46:12 -0300

    Att

--- palpites/models.py
+++ palpites/models.py
@@ -1,24 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
 
 class Time(models.Model):
     id = models.IntegerField(primary_key=True)
     Nome = models.CharField(max_length=30,unique=True)
-    escudo = models.FileField(upload_to='documents')
+    escudo = models.CharField(max_length=50,unique=True) # terá o endereço para a imagem
 
 class Partida(models.Model):
     id = models.IntegerField(primary_key=True)
     dia = models.DateField()
     rodada = models.IntegerField()
     ano = models.IntegerField()
-    Mandante = models.ForeignKey("Time",on_delete=models.CASCADE)
-    Visitante = models.ForeignKey("Time",on_delete=models.CASCADE)
+    Mandante = models.ForeignKey("Time",on_delete=models.CASCADE, related_name='mandante')
+    Visitante = models.ForeignKey("Time",on_delete=models.CASCADE, related_name='visitante')
     golsMandante = models.IntegerField(default=0)
     golsVisitante = models.IntegerField(default=0)
     vencedor = models.IntegerField()
 
-class Usuario(models.Model):
-    id = models.IntegerField(primary_key=True)
-    usuario = models.CharField(unique=True)
-    password = models.CharField()
+class Palpite_Partida(models.Model):
+    usuario = models.ForeignKey("User",on_delete=models.CASCADE)
+    partida = models.ForeignKey("Partida",on_delete=models.CASCADE)
+    golsMandante = models.IntegerField(default=0)
+    golsVisitante = models.IntegerField(default=0)
+    vencedor = models.IntegerField()
--- palpites/urls.py
+++ palpites/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from brasileirao.settings import STATIC_URL
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('icons/favicon.ico'))),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
