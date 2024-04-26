e10de59d33031756af9eaa4ae3841703d3cef6a2
ppinheirosiqueira <pedropinheirodesiqueira94@gmail.com>
2024-04-26 15:46:47 -0300

    Conversores para json separados no utils

--- palpites/utils/match.py
+++ palpites/utils/match.py
@@ -23,12 +23,3 @@ def get_anterior_proximo_partida(partida, time):
 
     return anterior, proximo
 
-def partida_to_json(partida):
-    return {
-        'pk': partida.pk,
-        'Mandante': partida.Mandante.id,
-        'Visitante': partida.Visitante.id,
-        'golsMandante': partida.golsMandante,
-        'golsVisitante': partida.golsVisitante,
-        'Rodada': f'{partida.Rodada.edicao_campeonato.campeonato} - {partida.Rodada.nome}',
-    }
